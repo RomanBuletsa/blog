a94b83e0e3ec06083043ffb17b17ba4de01d1cd4
RomanBuletsa <r.buletsa@gmail.com>
2017-11-12 15:45:12 +0200

    postcomments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,53 +1,52 @@
 class PostsController < ApplicationController
 before_action :authenticate_user!
-before_action :set_post, only: [:edit, :update, :destroy, :show]
+before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+def show
+	@comments = Postcomment.where(post_id: @post.id)
+end
+
+def new
+@post = Post.new
+end
+
+def create
+	@post = current_user.posts.build(post_params)
+	if @post.save
+	redirect_to root_path
+	else
+	render 'new'
+	end
+end
+
+def edit
 
 
-  def show
-  end
-
-  def new
-  @post = Post.new
-  end
-
-  def create
-  @post = current_user.posts.build(post_params)
-  if @post.save
-  	 redirect_to root_path
-  	else
-  		render 'new'
-  end
-  end
-
-  def edit
+end
 
 
-  end
+def update
+	@post.update(post_params)
+	redirect_to root_path
+end
 
 
-  def update	
-  @post.update(post_params)
-   	  redirect_to root_path
-  end
-
-
-  def destroy
-  	@post.destroy
- 	redirect_to root_path
-
-  end
+def destroy
+   @post.destroy
+   redirect_to root_path
+end
 
 
 
-  private 
+private
 
 def post_params
-    params.require(:post).permit(:user_id, :title, :body)
- end
+	params.require(:post).permit(:user_id, :title, :body)
+end
 
 
- def set_post 
- 	@post = Post.find(params[:id])
+def set_post
+	@post = Post.find(params[:id])
 end
 
 end
--- app/models/postcomment.rb
+++ app/models/postcomment.rb
@@ -2,7 +2,8 @@ class Postcomment < ApplicationRecord
 	belongs_to :user
 	belongs_to :post
 
-validates :text, length: { in: 1..400}
 validates :text, presence: true
+validates :text, length: { in: 1..400}
+validates :text, format: { with: /[a-zA-Z_#][\w.!?,&|@#$%^:;'"-]*/ }
 
 end
